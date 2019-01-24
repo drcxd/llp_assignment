b983094cd723d824ae36b5681deacb37024c1a7d
chang <drcxd@sina.com>
2019-01-24 20:30:58 +0800

    add print_uint function

--- io_library/test.asm
+++ io_library/test.asm
@@ -1,7 +1,7 @@
     global _start
     
     section .data
-test_string:    db "1234567890", 0
+test_string:    db "0123456789", 0
     section .text
 
 exit:
@@ -38,9 +38,41 @@ print_char:
     
     ret
 
-_start:
-    mov rdi, 0x41
+print_newline:
+    mov rdi, 0xA
     call print_char
+    ret
+
+print_uint:
+    mov rax, rdi
+    mov rcx, 10
+    dec rsp
+    mov byte[rsp], 0
+    .loop:
+    test rax, rax
+    jz .end
+    xor rdx, rdx
+    div rcx
+    dec rsp    
+    lea rdx, [test_string + rdx]
+    mov dl, byte [rdx]
+    mov byte [rsp], dl
+    jmp .loop
+
+    .end:
+    mov rdi, rsp
+    call print_string
+
+    mov rdi, rsp
+    call string_length
+    add rsp, rax
+    inc rsp
+    ret
+    
+
+_start:
+    mov rdi, 12345
+    call print_uint
     
     mov rdi, rax
     call exit
