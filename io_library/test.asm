ab9a357da48f5b183404fd59b97743a2172de9db
chang <drcxd@sina.com>
2019-01-25 10:42:19 +0800

    finish more routines

--- io_library/test.asm
+++ io_library/test.asm
@@ -68,11 +68,45 @@ print_uint:
     add rsp, rax
     inc rsp
     ret
+
+print_int:
+    ;; first the sign
+    cmp rdi, 0
+    jg .pos
+    push rdi
+    mov rdi, 0x2d
+    call print_char
+    pop rdi
+    neg rdi
+
+    .pos:    
+    call print_uint
+    
+    ret
+
+read_char:
+    dec rsp
+
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, rsp
+    mov rdx, 1
+    syscall
+
+    test rax, rax
+    jz .end
+    mov al, byte [rsp]
+    
+    .end:
+    inc rsp
+    ret
     
 
-_start:
-    mov rdi, 12345
-    call print_uint
+_start:    
+    call read_char
+
+    mov rdi, rax
+    call print_char
     
     mov rdi, rax
     call exit
