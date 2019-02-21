87315cd44c27bd86382628d76856e13a3e6ea0fb
chang <drcxd@sina.com>
2019-02-21 19:21:03 +0800

    add basic framework of the compiler

--- forth/forth.asm
+++ forth/forth.asm
@@ -13,9 +13,13 @@
     resq 1023
 rstack_start:   resq 1
 input_buf:  resb 1024
+user_mem:   resq 65536
+user_dict:  resq 65536
 
     section .data
+state:  dq 0
 last_word:  dq last
+here:   dq user_dict
 
     section .rodata
 msg_no_such_word:   db ": no such word", 10, 0
